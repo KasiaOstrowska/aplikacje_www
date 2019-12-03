1dab07a063bf6b3d6134d7ecf089cbbbf5e960d6
Katarzyna Ostrowska <ostr.kasia@wp.pl>
2019-12-04 00:36:42 +0100

    before migration

---
+++ kino/kinoCRUD/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+
+# Create your models here.
+class Kino():
+    nazwa=models.CharField(max_length=45)
+    iloscSal=models.IntegerField()
+    miejscowosc=models.CharField(max_length=45)
+
+
+class Film():
+    tytul= models.TextField()
+    rezyser= models.TextField()
+    czas= models.FloatField()
+    dataPremiert= models.DateField()
+
+class Seans():
+    dataSeansu=models.DateTimeField()
+    rodzajProjekcji=models.CharField(max_length=45)
+    idFilm=models.ForeignKey(Film,on_delete=models.CASCADE)
+    idSala=models.ForeignKey(Sala, on_delete=models.CASCADE)
+
+class Sala():
+    iloscMiejsc=models.IntegerField()
+    numerSal=models.IntegerField()
+    idKino=models.ForeignKey(Kino, on_delete=models.CASCADE)
+
+class Miejsce():
+    numerMiejsca=models.IntegerField()
+    rzad=models.CharField(max_length=3)
+    idSala=models.ForeignKey(Sala, on_delete=models.CASCADE)
+
+class Bilet():
+    idMiejsca=models.ForeignKey(Miejsce, on_delete=models.CASCADE)
+    cenaJednostkowa=models.FloatField()
+    rodzajBiletu=models.CharField(max_length=30)
+    idSeans=models.ForeignKey(Seans, on_delete=models.CASCADE)
+
+class Zamowienie():
+    cenaSprzedarzy=models.FloatField()
+    dataZamowienia=models.DateField()
+    status=models.CharField(max_length=20)
+    idKlient=models.ForeignKey(Uzytkownik, on_delete=models.PROTECT)
+
+class ZamowieniBilet():
+    idBilet=models.ForeignKey(Bilet, on_delete=models.CASCADE)
+    idZamowinie=models.ForeignKey(Zamowienie, on_delete=models.CASCADE)
+
+class Uzytkownik():
+    email=models.CharField(max_length=45)
+    haslo=models.CharField(max_length=45, min_length=8)
+    uprawnienia=models.CharField(max_length=50)
+    imie=models.CharField(max_length=45)
+    nazwisko=models.CharField(max_length=45)
+    stanowisko=models.CharField(blank=True, null=True, max_length=45)
+
+class KinoUser():
+    idKino=models.ForeignKey(Kino, on_delete=models.SET_NULL)
+    idUser=models.ForeignKey(Uzytkownik, on_delete=models.CASCADE)    
